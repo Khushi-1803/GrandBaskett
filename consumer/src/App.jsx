12c8be2ee96e4a2bf2780788450275e0b1bda238
Khushi-1803 <khushi011504@gmail.com>
2025-09-06 21:16:59 +0530

    fixed

--- consumer/src/App.jsx
+++ consumer/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 
 import {Toaster} from "react-hot-toast"
 import Footer from './components/Footer'
-import { useAppContext } from './context/AppContext'
+import { useAppContext } from './context/AppContext' 
 import Login from './components/Login' 
 import AllProducts from './pages/AllProducts'
 import ProductCategory from './pages/ProductCategory'
@@ -20,6 +20,8 @@ import ProductList from './pages/seller/ProductList'
 import Orders from './pages/seller/Orders'
 import Loading from './components/Loading'
 import Contact from './pages/contact'
+
+
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller")
   const {showUserLogin,isSeller} = useAppContext()
@@ -30,8 +32,8 @@ const App = () => {
      <Toaster/>
      <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/contact' element={<Contact />}/> 
         <Route path='/products' element={<AllProducts/>}/>
         <Route path='/products/:category' element={<ProductCategory/>}/>
         <Route path='/products/:category/:id' element={<ProductDetails/>}/>
@@ -53,3 +55,4 @@ const App = () => {
 }
 
 export default App
+
